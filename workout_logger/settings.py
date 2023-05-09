ea6bcf5c782653b52ce882be678eca982053f4b8
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2023-05-10 01:50:08 +0400

    update project settings

--- workout_logger/settings.py
+++ workout_logger/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u68%_z_z0dr1r!7tkuanw^yl9%nbzjq7ak1idj6x$2cv87i4ld'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'workoutApp',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
